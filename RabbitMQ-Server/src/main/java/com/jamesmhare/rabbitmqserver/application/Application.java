d3173919545ce2a36be6c4a7806abdf78bf37f9b
James Hare <james@notmyrealemail.com>
2018-12-09 18:39:37 -0600

    RABMQ-10001 Added modules for RabbitMQ Server, Publisher and Subscriber.
    Updated .gitignore
    
    Delete .project file
    
    Delete .settings folder

---
+++ RabbitMQ-Server/src/main/java/com/jamesmhare/rabbitmqserver/application/Application.java
@@ -0,0 +1,9 @@
+package com.jamesmhare.rabbitmqserver.application;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ RabbitMQ-Subscriber/src/main/java/com/jamesmhare/rabbitmqsubscriber/application/Application.java
@@ -0,0 +1,9 @@
+package com.jamesmhare.rabbitmqsubscriber.application;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+	}
+
+}
